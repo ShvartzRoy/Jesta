ccfbf574722adb64ab1cb05ae1c303e31fcc2d98
linamass <linamasarwa7@gmail.com>
2024-12-31 03:38:51 +0200

    added service model

--- jestaApi/services/models.py
+++ jestaApi/services/models.py
@@ -1,12 +1,51 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
-# class service(models.Model):
-#     title = models.CharField(max_length=100)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=5, decimal_places=2)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    publisher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="published_services",
+        on_delete=models.CASCADE
+    )
     
-#     def __str__(self):
-#         return self.name
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name="services")
+    location = models.CharField(max_length=255)
+    date_time_range = models.JSONField()  
+    estimated_duration = models.DurationField()
+    
+    applicants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="applied_services",
+        blank=True
+    )
+    
+    state = models.CharField(
+        max_length=20,
+        choices=[("pending", "Pending"), ("accepted", "Accepted"), ("inProgress", "In Progress"), ("completed", "Completed")],
+        default="pending"
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publisher})"
+
+
+class JobService(Service):
+    offered_payment = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class FreeService(Service):
+    pass
+
+
+class VolunteeringService(Service):
+    pass
