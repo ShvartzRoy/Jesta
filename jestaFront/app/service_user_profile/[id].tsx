ef704e06e3f186158774cbfbc811f506f10a78e7
Shvartz-Roy <102583578+ShvartzRoy@users.noreply.github.com>
2025-04-08 19:09:15 +0300

    solve

--- jestaFront/app/service_user_profile/[id].tsx
+++ jestaFront/app/service_user_profile/[id].tsx
@@ -126,21 +126,6 @@ useEffect(() => {
     setRefreshTrigger(prev => prev + 1);
   };
 
-  const handleStartChat = async () => {
-      try {
-        const response = await axios.post(
-          `${process.env.EXPO_PUBLIC_HOST}/api/chats/initiate_chat`,
-          { other_user_id: userId },
-          { headers: { Authorization: `Bearer ${user.token}` } }
-        );
-        if (response.status === 200) {
-          router.push("/chat");
-        }
-      } catch (err) {
-        console.error("Error initiating chat", err);
-        Alert.alert("Error", "Failed to start chat");
-      }
-    };
   
   //-------------------------------
 
